dc3d070700dd411eaebc18c76f52d8a2c668caa5
juanmendez@gkudos.com <juanmendez@gkudos.com>
2018-11-24 05:59:20 -0500

    cálculo de frecuencias

--- lab_divulgaciondatos/ScriptTarea.R
+++ lab_divulgaciondatos/ScriptTarea.R
@@ -7,7 +7,21 @@ ncol(df2)
 
 print(df2)
 
+# indice de la columnas
+print ( grep("w", colnames(df2)) )
 
-ff <- freqCalc(df2, keyVars=c(1,2,3,4) , w=34) 
-print(cbind(x,ff$fk,ff$Fk))
+print ( grep("school", colnames(df2)) )
+print ( grep("sex", colnames(df2)) )
+print ( grep("address", colnames(df2)) )
+
+#ff <- freqCalc(df2, keyVars=c(2,3,5) , w=35) 
+#print(cbind(df2,ff$fk,ff$Fk))
+
+# calcular frecuencias de las tipo category 
+
+ff <- freqCalc(df2, keyVars=c('school', 'sex', 'address', 'famsize', 'Pstatus', 'Mjob', 'Fjob',
+                              'reason', 'guardian', 'schoolsup', 'famsup', 'paid', 'activities',
+                              'nursery', 'higher', 'internet', 'romantic') , w=35) 
+print(cbind(df2,ff$fk,ff$Fk))
+
 
